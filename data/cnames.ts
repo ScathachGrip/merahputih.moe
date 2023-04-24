c22f21d52cec99a0f95b2111e3c246978b9a65b3
sinkaroid <bibsindra@gmail.com>
2023-04-24 09:12:24 +0700

    feat: add cnames prototype

---
+++ data/cnames.ts
@@ -0,0 +1,7 @@
+import { ICnames } from "./interfaces";
+
+export const data: ICnames = {
+  active: {
+    "scathach": "scathachgrip.github.io",
+  }
+};
---
+++ data/interfaces.ts
@@ -0,0 +1,5 @@
+export interface ICnames {
+    active: {
+        [key: string]: string;
+    }
+};
