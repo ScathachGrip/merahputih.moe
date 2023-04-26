340ee48041348d03b1d5162df403c3e59a91d15f
sinkaroid <bibsindra@gmail.com>
2023-04-26 19:43:44 +0700

    ci restricted check

--- test/pull_requests.ts
+++ test/pull_requests.ts
@@ -1,6 +1,7 @@
 import chai from "chai";
 
 import { data } from "../src/cnames";
+import { list } from "../src/restricted";
 const expect = chai.expect;
 const keys = Object.keys(data.active);
 
@@ -8,7 +9,17 @@ describe("Validating Cnames", () => {
   it("should be in alphabetical order", () => {
     for (let i = 0; i < keys.length - 1; i++) {
       if (keys[i] > keys[i + 1]) {
-        expect.fail("Cnames are not in alphabetical order");
+        expect.fail(`Cname "${keys[i]}" is out of order`);
+      }
+    }
+  });
+
+  it("should not be in the restricted list", () => {
+    for (let i = 0; i < keys.length; i++) {
+      for (let j = 0; j < list.list.length; j++) {
+        if (keys[i].match(list.list[j])) {
+          expect.fail(`Cname "${keys[i]}" is in the restricted list`);
+        }
       }
     }
   });
