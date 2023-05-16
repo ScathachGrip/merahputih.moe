7fd7e9205b77f3fe5aea4ecf1fae2b27585ebfe3
Indrawan I <bibsindra@gmail.com>
2023-05-16 17:08:27 +0700

    janda.merahputih.moe

--- src/cnames.ts
+++ src/cnames.ts
@@ -3,6 +3,7 @@ import { ICnames } from "./interfaces";
 export const data: ICnames = {
   active: {
     "indra": "sinkaroid.github.io/me",
+    "janda": "sinkaroid.github.io/jandapress",
     "scathach": "scathachgrip.github.io",
   }
 };
