4703acff0dbe78e9634169e3c9d6edbca50ca673
Indrawan I <bibsindra@gmail.com>
2023-05-16 17:18:01 +0700

    lust.merahputih.moe (#12)

--- src/cnames.ts
+++ src/cnames.ts
@@ -4,6 +4,7 @@ export const data: ICnames = {
   active: {
     "indra": "sinkaroid.github.io/me",
     "janda": "sinkaroid.github.io/jandapress",
+    "lust": "sinkaroid.github.io/lustpress",
     "scathach": "scathachgrip.github.io",
   }
 };
