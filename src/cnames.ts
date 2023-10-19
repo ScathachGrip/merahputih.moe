71659172c54324682c5120fa76a495352b0aafe5
Yoga Pranata <ahmadzaylani001@gmail.com>
2023-10-19 17:45:46 +0700

    wibuapi.merahputih.moe & zyxdevs.merahputih.moe (#16)
    Register wibuapi.merahputih.moe and zyxdevs.merahputih.moe

--- src/cnames.ts
+++ src/cnames.ts
@@ -6,5 +6,7 @@ export const data: ICnames = {
     "janda": "sinkaroid.github.io/jandapress",
     "lust": "sinkaroid.github.io/lustpress",
     "scathach": "scathachgrip.github.io",
+    "wibuapi": "wibuapi.onrender.com",
+    "zyxdevs": "zyxdevs.github.io/zYxDevs",
   }
 };
