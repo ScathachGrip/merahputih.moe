ac25988833ce8df8aecc4e5ae15d2046923d2af1
Risky <risky@kazuo.rf.gd>
2024-01-12 17:38:06 +0700

    Update cnames.ts

--- src/cnames.ts
+++ src/cnames.ts
@@ -6,8 +6,8 @@ export const data: ICnames = {
     "janda": "sinkaroid.github.io/jandapress",
     "lust": "sinkaroid.github.io/lustpress",
     "scathach": "scathachgrip.github.io",
-    "wibuapi": "wibuapi.onrender.com",
     "ryuxyro": "ryuxyro.github.io/moe",
+    "wibuapi": "wibuapi.onrender.com",
     "zyxdevs": "zyxdevs.github.io/zYxDevs",
   }
 };
