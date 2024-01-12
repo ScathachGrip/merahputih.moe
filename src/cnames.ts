dcd7a37c9a922234bda9178147343002c1531b6f
Risky <risky@kazuo.rf.gd>
2024-01-12 17:33:35 +0700

    Update cnames.ts

--- src/cnames.ts
+++ src/cnames.ts
@@ -2,12 +2,12 @@ import { ICnames } from "./interfaces";
 
 export const data: ICnames = {
   active: {
-    "ryuxyro": "ryuxyro.github.io/RyuXyro",
     "indra": "stupendous-unicorn-65a376.netlify.app",
     "janda": "sinkaroid.github.io/jandapress",
     "lust": "sinkaroid.github.io/lustpress",
     "scathach": "scathachgrip.github.io",
     "wibuapi": "wibuapi.onrender.com",
     "zyxdevs": "zyxdevs.github.io/zYxDevs",
+    "ryuxyro": "ryuxyro.github.io/RyuXyro",
   }
 };
